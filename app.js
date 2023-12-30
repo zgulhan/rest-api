6e7b0af52c1f7d8aab5f625b580d470553d444c5
Zeynep Gulhan <grouchysmurfz@gmail.com>
2023-12-30 21:52:25 +0300

    db change

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ app.use(cors({
 
 
 //mongoose.connect("mongodb+srv://my-rest-api:" + process.env.MONGO_ATLAS_PW + "@my-rest-api.9nx2ruj.mongodb.net/");
-const dblink = process.env.DBSTART + process.env.MONGO_ATLAS_PW + process.env.DBEND;
-mongoose.connect(dblink);
+const dburl = process.env.DBURL;
+mongoose.connect(dburl);
 
 
 app.use(morgan("dev"));
